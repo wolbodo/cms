02208fc624cf2bc37dcb05b792211fb6cb8d6d60
Dexter <dexter@dxlb.nl>
2023-03-31 20:12:41 +0200

    Adding pages

---
+++ src/api/zaalverhuur/controllers/zaalverhuur.ts
@@ -0,0 +1,7 @@
+/**
+ * zaalverhuur controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::zaalverhuur.zaalverhuur');
---
+++ src/api/zaalverhuur/routes/zaalverhuur.ts
@@ -0,0 +1,7 @@
+/**
+ * zaalverhuur router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::zaalverhuur.zaalverhuur');
